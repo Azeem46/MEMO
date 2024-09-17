2711d539931f972eaa6752bc5469b08a2a0657ce
Azeem46 <shaikhazeem4646@gmail.com>
2024-09-17 15:23:25 +0530

    corrected logic for server

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import rateLimit from "express-rate-limit";
-import cron from "node-cron";
+// import rateLimit from 'express-rate-limit';
 import postRoutes from "./routes/postsRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import commentRoute from "./routes/commentsRoutes.js";
@@ -13,32 +12,31 @@ import bookmarkRoutes from "./routes/bookmarkRoutes.js";
 const app = express();
 dotenv.config();
 
-// Rate limiter configuration
-const apiLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour
-  max: 100, // Limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again later.",
-});
+// // Rate limiter configuration
+// const apiLimiter = rateLimit({
+//   windowMs: 60 * 60 * 1000, // 1 hour
+//   max: 100, // Limit each IP to 100 requests per windowMs
+//   message: 'Too many requests from this IP, please try again later.',
+// });
 
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
-// Correct CORS setup for allowed origins
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://memo-five-beta.vercel.app"], // Allow multiple origins
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-  })
-);
-
-app.use(apiLimiter); // Apply rate limiter to all routes
+app.use(cors());
+// app.use(apiLimiter); // Apply rate limiter to all routes
 
 // Routes
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 app.use("/comments", commentRoute);
 app.use("/bookmarks", bookmarkRoutes);
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // or the port your Vite frontend is running on
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
@@ -52,9 +50,3 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
-
-// Cron job configuration
-// Run every 30 minutes
-cron.schedule("*/30 * * * *", () => {
-  console.log("Pinging the server to keep it awake");
-});
