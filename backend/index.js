39b1aa2057e9d36d3329be86a2f46309c6fb33f6
Azeem46 <shaikhazeem4646@gmail.com>
2024-09-17 15:06:06 +0530

    added cron job and app limiter to backend

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import rateLimit from 'express-rate-limit';
+import rateLimit from "express-rate-limit"; // uncommented
+import cron from "node-cron"; // Add node-cron
 import postRoutes from "./routes/postsRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import commentRoute from "./routes/commentsRoutes.js";
@@ -12,31 +13,31 @@ import bookmarkRoutes from "./routes/bookmarkRoutes.js";
 const app = express();
 dotenv.config();
 
-// // Rate limiter configuration
-// const apiLimiter = rateLimit({
-//   windowMs: 60 * 60 * 1000, // 1 hour
-//   max: 100, // Limit each IP to 100 requests per windowMs
-//   message: 'Too many requests from this IP, please try again later.',
-// });
+// Rate limiter configuration
+const apiLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+});
 
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-// app.use(apiLimiter); // Apply rate limiter to all routes
+app.use(apiLimiter); // Apply rate limiter to all routes
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Adjust based on where your frontend is running
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
 
 // Routes
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 app.use("/comments", commentRoute);
 app.use("/bookmarks", bookmarkRoutes);
-
-app.use(
-  cors({
-    origin: "http://localhost:5173", // or the port your Vite frontend is running on
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-  })
-);
 
 const PORT = process.env.PORT || 5000;
 
@@ -50,3 +51,8 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
+
+// Cron job configuration
+cron.schedule("0 0 * * *", () => {
+  console.log("Running a task every day at midnight");
+});
