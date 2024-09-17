9996db7a5a651d50752dcdfdbc87c9e14531192c
Azeem46 <shaikhazeem4646@gmail.com>
2024-09-17 15:25:47 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import rateLimit from 'express-rate-limit';
 import postRoutes from "./routes/postsRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import commentRoute from "./routes/commentsRoutes.js";
@@ -12,31 +11,27 @@ import bookmarkRoutes from "./routes/bookmarkRoutes.js";
 const app = express();
 dotenv.config();
 
-// // Rate limiter configuration
-// const apiLimiter = rateLimit({
-//   windowMs: 60 * 60 * 1000, // 1 hour
-//   max: 100, // Limit each IP to 100 requests per windowMs
-//   message: 'Too many requests from this IP, please try again later.',
-// });
-
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-// app.use(apiLimiter); // Apply rate limiter to all routes
+
+// CORS configuration
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Vite local development
+      "https://memo-five-beta.vercel.app", // Vercel production
+    ],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true, // If you're using cookies for authentication
+  })
+);
 
 // Routes
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 app.use("/comments", commentRoute);
 app.use("/bookmarks", bookmarkRoutes);
-
-app.use(
-  cors({
-    origin: "http://localhost:5173", // or the port your Vite frontend is running on
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-  })
-);
 
 const PORT = process.env.PORT || 5000;
 
